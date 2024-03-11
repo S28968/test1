00bd44dfa8efc9c0f8fddc3da8ef41e15c26d7e6
test <s28968@pjwstk.edu.pl>
2024-03-11 12:57:27 +0100

    Initial project

---
+++ consoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿
+class Program
+{
+    public static void Main(string[]args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        int[] tbl = { 1, 2, 3, 4 };
+
+        foreach (int num in tbl)
+        {
+            Console.WriteLine(num);
+        }
+    }
+}
